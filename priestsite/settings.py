aa9b9b4c83901cb7910ef72ec1d301a3d41862f0
Ceecosmo <153816310+Ceecosmo@users.noreply.github.com>
2024-04-28 13:18:54 +0100

    Update settings.py

--- priestsite/settings.py
+++ priestsite/settings.py
@@ -99,15 +99,19 @@ WSGI_APPLICATION = 'priestsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES["default"] = dj_database_url.parse("postgres://priestwelfarehome_user:BDH5GIT5iNzkZDf4ew53UwJsXM9Oq5V0@dpg-combjlgl6cac73d4qc1g-a.oregon-postgres.render.com/priestwelfarehome")
+
+database_url = os.environ.get("DATABASE_URL")
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(database_url)
 }
-
-DATABASES["default"] = dj_database_url.parse("postgres://priestwelfarehome_user:BDH5GIT5iNzkZDf4ew53UwJsXM9Oq5V0@dpg-combjlgl6cac73d4qc1g-a.oregon-postgres.render.com/priestwelfarehome")
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
